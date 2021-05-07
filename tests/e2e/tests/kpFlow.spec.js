a800f31c85ee61d7290df0de129764f5b963aa85
Kontrazen <krlovic@gmail.com>
2021-05-07 14:43:30 +0200

    Initial complete checkout flow

--- tests/e2e/tests/kpFlow.spec.js
+++ tests/e2e/tests/kpFlow.spec.js
@@ -1,5 +1,6 @@
 import puppeteer from "puppeteer";
 import kpURLS from "../helpers/kpURLS";
+import kpFrame from "../helpers/kpFrame";
 
 import {
 	puppeteerOptions as options,
@@ -100,8 +101,19 @@ const selectedPaymentMethod = invoicePaymentMethod;
 // Coupon selection
 const appliedCoupons = [couponPercent];
 
-// Shipping in KCO iFrame ("yes") / Standard WC ("no")
-const iframeShipping = "yes";
+//------------- 1
+/**
+ *
+ * @param page
+ * @param name
+ * @returns {Promise<*|Frame>}
+ */
+ const loadIFrame = async (page, name) =>
+ page.frames().find((frame) => frame.name() === name);
+//------------- 1XXX
+
+
+
 
 /**
  * TEST INITIALIZATION
@@ -138,9 +150,91 @@ describe("KP", () => {
 	 * Begin test suite
 	 */
 	test("second flow should be on the my account page", async () => {
-		await page.goto("http://localhost:8000");
+		// await page.goto("http://localhost:8000");
+		await page.goto("http://localhost:8000/shop/?add-to-cart=1547");
+		await page.waitForTimeout(1000);
+		await page.goto("http://localhost:8000/checkout");
+
+
+
+		await page.waitForSelector('label[for="payment_method_klarna_payments_pay_later"]');
+		await page.evaluate(
+			(cb) => cb.click(),
+			await page.$('label[for="payment_method_klarna_payments_pay_later"]')
+		);
+
+
+//---------------------o 2
+
+await kpFrame.submitBillingForm(page, billingData);
+
+//---------------------x 2
+
+
 		await page.waitForTimeout(2000);
+
+		const framePayLater = await loadIFrame(
+			page,
+			"klarna-pay-later-main"
+		);
+
+		// await framePayLater.waitForSelector('label[for="installments-invoice|-1"]');
+		// await framePayLater.evaluate(
+		// 	(cb) => cb.click(),
+		// 	await framePayLater.$('label[for="installments-pix|3504"]')
+		// );
+
+		// await page.waitForTimeout(500);
+
+		await framePayLater.evaluate(
+			(cb) => cb.click(),
+			await framePayLater.$('label[for="installments-invoice|-1"]')
+		);
+
+		await framePayLater.evaluate(
+			(cb) => cb.click(),
+			await framePayLater.$('label[for="installments-invoice|-1"]')
+		);
+
+		// await page.waitForTimeout(500);
+
+		await page.evaluate(
+			(cb) => cb.click(),
+			await page.$('input[type="checkbox"][name="terms"][id="terms"]')
+		);
+
+		await page.waitForTimeout(500);
+
+		await page.evaluate(
+			(cb) => cb.click(),
+			await page.$('button[type="submit"][name="woocommerce_checkout_place_order"][id="place_order"]')
+		);
+
+		await page.waitForTimeout(1000);
+
+		// PIN NUMBER
+		const framePayLaterFullScreen = await loadIFrame(
+			page,
+			"klarna-pay-later-fullscreen"
+		);
+
+		await page.evaluate(
+			(cb) => cb.click(),
+			await page.$('button[type="submit"][name="woocommerce_checkout_place_order"][id="place_order"]')
+		);
+
+		await framePayLaterFullScreen.evaluate(
+			(cb) => cb.click(),
+			await framePayLaterFullScreen.$('input[name="nationalIdentificationNumber"]')
+		);
+		await framePayLaterFullScreen.type('input[name="nationalIdentificationNumber"]', "410321-9202");
+
+		await framePayLaterFullScreen.evaluate(
+			(cb) => cb.click(),
+			await framePayLaterFullScreen.$('button[id="purchase-approval-form-continue-button"]')
+		);
+
 		const h1 = await page.$eval("h1", (e) => e.textContent);
-		expect(h1).toBe("Welcome");
+		expect(h1).toBe("Checkout");
 	}, 190000);
 });
