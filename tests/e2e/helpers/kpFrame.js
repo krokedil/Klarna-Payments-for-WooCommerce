44def181ead11045d476f15a1e33efa1bccea7ba
Kontrazen <krlovic@gmail.com>
2021-07-16 13:17:07 +0200

    Update frame manipulkaation logic

--- tests/e2e/helpers/kpFrame.js
+++ tests/e2e/helpers/kpFrame.js
@@ -1,13 +1,11 @@
-
 /**
  *
  * @param page
  * @param name
  * @returns {Promise<*|Frame>}
  */
- const loadIFrame = async (page, name) =>
- page.frames().find((frame) => frame.name() === name);
-
+const loadIFrame = async (page, name) =>
+	page.frames().find((frame) => frame.name() === name);
 
 /**
  *
@@ -16,7 +14,7 @@
  * @returns {Promise<void>}
  */
 
- const submitBillingForm = async (frame, data) => {
+const submitBillingForm = async (frame, data) => {
 	const {
 		emailSelector,
 		email,
@@ -26,51 +24,51 @@
 		firstName,
 		lastNameSelector,
 		lastName,
+		companySelector,
+		companyName,
 		telephoneSelector,
 		telephone,
-        cityNameSelector,
-        cityName,
-        billingAddressSelector,
-        billingAddress
-
+		cityNameSelector,
+		cityName,
+		billingAddressSelector,
+		billingAddress,
 	} = data;
 
 	// Fill out input field
 	const fillOutFrameField = async (fieldName, inputFieldFillIn) => {
-
-		if (await frame.$(fieldName)){
-
-			let inputField = await frame.$(fieldName);
-			await inputField.click({clickCount: 3});
-			await inputField.press('Backspace');
+		if (await frame.$(fieldName)) {
+			const inputField = await frame.$(fieldName);
+			await inputField.click({ clickCount: 3 });
+			await inputField.press("Backspace");
 			await frame.waitForTimeout(100);
 			await inputField.type(inputFieldFillIn);
 			await frame.waitForTimeout(200);
 		}
-	}
+	};
 
 	// Fill out the form
 	const completeForm = async () => {
 		await fillOutFrameField(firstNameSelector, firstName);
 		await fillOutFrameField(lastNameSelector, lastName);
+		await fillOutFrameField(companySelector, companyName);
 		await fillOutFrameField(emailSelector, email);
 		await fillOutFrameField(telephoneSelector, telephone);
 		await fillOutFrameField(postalCodeSelector, postalCode);
 		await fillOutFrameField(cityNameSelector, cityName);
 		await fillOutFrameField(billingAddressSelector, billingAddress);
-	}
-	
+	};
+
 	// Check for miniaturized frame
-		if(	await frame.$('[id="preview__link"]')) {
-			await frame.waitForTimeout(1000);
-			await frame.click('[id="preview__link"]');
-			await frame.waitForTimeout(1000);
-		}
-	
-        await completeForm();
+	if (await frame.$('[id="preview__link"]')) {
+		await frame.waitForTimeout(1000);
+		await frame.click('[id="preview__link"]');
+		await frame.waitForTimeout(1000);
+	}
+
+	await completeForm();
 };
 
 export default {
-    loadIFrame,
-    submitBillingForm,
-}
+	loadIFrame,
+	submitBillingForm,
+};
